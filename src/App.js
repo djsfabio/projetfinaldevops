22a79e58d82e957b3f25e0da70bad4363f4147a4
fabiodjs <pro.fabiodjs@gmail.com>
2021-05-07 17:56:34 +0200

    Modification d'App.js pour vérifier que le fichier YML de Docker fonctionne.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello world
+          Bonjour je m'appelle Fabio
         </a>
       </header>
     </div>
